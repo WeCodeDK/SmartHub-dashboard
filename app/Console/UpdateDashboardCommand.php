b2d11ecbc4c32915fc30fd921d0c92d49dd88372
alexander_wecode <alexander@wecode.dk>
2018-12-28 02:03:58 +0100

    Update UpdateDashboardCommand.php

--- app/Console/UpdateDashboardCommand.php
+++ app/Console/UpdateDashboardCommand.php
@@ -13,14 +13,10 @@ class UpdateDashboardCommand extends Command
     public function handle()
     {
         $this->call('dashboard:determine-appearance');
-       // $this->call('dashboard:fetch-train-connections');
+        $this->call('dashboard:dashboard:fetch-lunch');
+        $this->call('dashboard:fetch-train-rejseplan');
         $this->call('dashboard:send-heartbeat');
-       // $this->call('dashboard:fetch-current-tracks');
-       // $this->call('dashboard:fetch-velo-stations');
-       // $this->call('dashboard:fetch-tasks');
-       // $this->call('dashboard:fetch-team-member-status');
-       // $this->call('dashboard:fetch-calendar-events');
-       // $this->call('dashboard:fetch-github-totals');
-       // $this->call('dashboard:fetch-packagist-totals');
+
+
     }
 }
